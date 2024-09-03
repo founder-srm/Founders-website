feeab233a48e407983d11be789b879828b99556d
Saransh Bangar <saranshbangad@gmail.com>
2024-09-03 12:57:58 +0530

    Added navbar and sidebar

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+import Sidebar from '../components/sidebar'
+
+const page = () => {
+  return (
+    <main>
+      <Navbar />
+      <Sidebar />
+
+      {/* write the about page code here */}
+      <section className="pl-[60px] pt-[60px]">
+      </section>
+    </main>
+  )
+}
+
+export default page
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -58,6 +58,9 @@ const config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        'noto-serif': ['"Noto Serif"', 'serif'],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -75,6 +78,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
