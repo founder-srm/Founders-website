2f0d1bde3edce635eb5b74e1d7e021be520e5d51
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-09-05 19:27:13 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { type NextRequest } from 'next/server';
-import { createClient } from '@/utils/supabase/middleware';
+import { updateSession } from '@/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  return await createClient(request);
+  return await updateSession(request);
 }
 
 export const config = {
