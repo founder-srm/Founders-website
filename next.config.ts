5cd64c4d04bc895cf087a0e70b149d7e4e10e25f
Suvan Gowri Shanker <118198968+greeenboi@users.noreply.github.com>
2025-09-03 13:33:12 +0000

    feat: updated to latest minor release for nextjs

--- next.config.ts
+++ next.config.ts
@@ -2,41 +2,36 @@
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'eedplvopkhwuhhquagfw.supabase.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'shadcnblocks.com',
-      },
-    ],
-  },
-  transpilePackages: ['next-mdx-remote'],
-};
-
 const withMDX = createMDX({
+  // Match both .md and .mdx
+  extension: /\.mdx?$/,
   // Add markdown plugins here, as desired
   // options: {
   //   remarkPlugins: [remarkGfm],
   //   rehypePlugins: [],
   // },
 });
+
+const nextConfig: NextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.sanity.io' },
+      { protocol: 'https', hostname: 'eedplvopkhwuhhquagfw.supabase.co' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'shadcnblocks.com' },
+    ],
+  },
+  transpilePackages: ['next-mdx-remote'],
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  }
+};
 
 export default withMDX(nextConfig);
