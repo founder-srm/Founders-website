f494175e29060b8dba8f06cd2abe7111a68c718f
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-09-04 14:18:48 +0530

    fix: initialize timeoutRef with null in useDebounce for improved type safety

--- src/lib/performance.ts
+++ src/lib/performance.ts
@@ -10,7 +10,7 @@ export function useDebounce<T extends (...args: never[]) => unknown>(
   callback: T,
   delay: number
 ): T {
-  const timeoutRef = useRef<NodeJS.Timeout>();
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   return useCallback(
     ((...args: Parameters<T>) => {
